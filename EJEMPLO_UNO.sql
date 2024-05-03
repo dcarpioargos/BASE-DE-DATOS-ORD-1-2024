45d1dac5856c690bd3663367745820ac6fc09ccb
carpiordaniel <carpiordaniel@gmail.com>
2024-05-02 20:58:05 -0500

    crear tablas

--- EJEMPLO_UNO.sql
+++ EJEMPLO_UNO.sql
@@ -1,4 +1,30 @@
-use sys
-SELECT * FROM sys_config
+/*use sys
+SELECT * FROM sys_config*/
 
-s;ojhhidgjkshhdjfsnd
+-- SYNTAXIS -- PASOS O ESTRUCTURA A SEGUIR
+-- CREAR BD - DB
+CREATE DATABASE materia_db;
+
+-- seter por default bd
+USE materia_db
+-- CREAR TABLAS
+
+CREATE TABLE alumnos (
+	
+    nombres VARCHAR(75),
+    apellidos VARCHAR(75),
+	edad INT,
+    isActive BOOLEAN,
+    fech_nac DATE
+)
+
+
+
+ 
+
+ 
+
+
+
+
+
