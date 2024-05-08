7b4d2dfed4f4e88e9bbfde8b1843875e62f77b03
carpiordaniel <carpiordaniel@gmail.com>
2024-05-07 20:59:46 -0500

    insert

--- EJEMPLO_UNO.sql
+++ EJEMPLO_UNO.sql
@@ -10,7 +10,7 @@ USE materia_db
 -- CREAR TABLAS
 
 CREATE TABLE alumnos (
-	
+	-- id
     nombres VARCHAR(75),
     apellidos VARCHAR(75),
 	edad INT,
@@ -18,11 +18,36 @@ CREATE TABLE alumnos (
     fech_nac DATE
 )
 
+INSERT INTO alumnos (apellidos, edad, isActive, fech_nac)
+	VALUES ("CAICEDO2", 50, "0", "2024/01/24"),
+		   ("ISABEL", "CAICEDO2", 50, "0", "2024/01/24"),
+	       ("ISABEL", "CAICEDO2", 50, "0", "2024/01/24");
+
+SELECT * FROM alumnos
+  
+
+
 
 
  
+union
+union all
 
+
+
+
+
+CREATE TABLE clase1(
+	MATERIA varchar(75),
+    PROMEDIO int(3),
+	CRITERIO BOOL
+)
  
+ INSERT INTO clase1(MATERIA, PROMEDIO, CRITERIO)
+	VALUES ("Base de Datos", 100, true),
+			("Programacion", 90, false);
+            
+SELECT * FROM clase1
 
 
 
